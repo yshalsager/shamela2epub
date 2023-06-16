1d348e72f54d0fbd8840357f4019b092667ef35a
yshalsager <ysh-alsager@hotmail.com>
2023-06-16 23:00:46 +0300

    feat: Add Parallel Downloads
    * set httpx log level to warning
    * fix two gui bugs: disabled input after error and reusing app QThread after one task
    
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- shamela2epub/gui/app.py
+++ shamela2epub/gui/app.py
@@ -4,8 +4,9 @@ from typing import cast
 
 import click
 import qdarktheme
+from gevent import Greenlet, joinall, spawn
 from PyQt5 import uic
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtCore import QObject, QRunnable, QThreadPool, pyqtSignal
 from PyQt5.QtGui import QFontDatabase
 from PyQt5.QtWidgets import (
     QApplication,
@@ -20,32 +21,59 @@ from PyQt5.QtWidgets import (
 from shamela2epub import PKG_DIR
 from shamela2epub.main import BookDownloader
 from shamela2epub.misc.utils import browse_file_directory
+from shamela2epub.models.book_html_page import BookHTMLPage
 
 
-class Worker(QObject):
+class QBookDownloader(BookDownloader):
+    def __init__(self, url: str) -> None:
+        super().__init__(url, connections=10)
+        self.progress: pyqtSignal = pyqtSignal(str)
+
+    def download_page(self, page_number: int) -> BookHTMLPage:
+        with self._sem:
+            book_html_page = BookHTMLPage(f"{self.url}/{page_number}")
+            self.progress.emit(
+                f"تحميل الصفحة {page_number} من {self.epub_book.pages_count}"
+            )
+        return book_html_page
+
+    def download(self) -> None:
+        self.progress.emit(f"تحميل الصفحة 1 من {self.epub_book.pages_count}")
+        self.create_first_page()
+        jobs = [
+            spawn(self.download_page, page_number)
+            for page_number in range(2, self.epub_book.pages_count + 1)
+        ]
+        job: Greenlet
+        for job in joinall(jobs):
+            self.epub_book.add_page(job.value)
+
+
+class WorkerSignals(QObject):
     finished = pyqtSignal()
     progress = pyqtSignal(str)
     downloaded = pyqtSignal(Path)
 
-    def __init__(self, downloader: BookDownloader, output: str) -> None:
-        self.downloader: BookDownloader = downloader
-        self.output: str = output
+
+class Worker(QRunnable):
+    def __init__(self, downloader: QBookDownloader, output: str) -> None:
         super().__init__()
+        self.downloader: QBookDownloader = downloader
+        self.output: str = output
+        self.signals = WorkerSignals()
 
     def run(self) -> None:
         """Process the book."""
+        self.downloader.progress = self.signals.progress
         self.downloader.create_info_page()
-        self.progress.emit(
+        self.signals.progress.emit(
             f"بدء العمل على كتاب {self.downloader.book_info_page.title} لمؤلفه {self.downloader.book_info_page.author}"
         )
-        self.downloader.create_first_page()
-        self.progress.emit("إنشاء الصفحة الأولى")
-        for page in self.downloader.download():
-            self.progress.emit(f"إنشاء الصفحة {page}")
-        self.progress.emit("حفظ الكتاب")
+        self.downloader.download()
+        self.signals.progress.emit("حفظ الكتاب")
         output_book = self.downloader.save_book(self.output)
-        self.downloaded.emit(output_book)
-        self.finished.emit()
+        self.signals.downloaded.emit(output_book)
+        self.signals.finished.emit()
 
 
 class App(QMainWindow):
@@ -56,8 +84,7 @@ class App(QMainWindow):
     def __init__(self) -> None:
         """GUI App constructor."""
         super().__init__()
-        self.thread: QThread = QThread()
-        self.worker: Worker | None = None
+        self.thread_pool: QThreadPool = QThreadPool()
 
         uic.loadUi(f"{PKG_DIR}/gui/ui.ui", self)
         # self.setWindowIcon(QIcon(f"{WORK_DIR}/assets/books-duotone-512.png"))
@@ -105,14 +132,21 @@ class App(QMainWindow):
     def report_progress(self, progress: str) -> None:
         self.update_statusbar(progress)
 
+    def on_finish(self) -> None:
+        self.download.setEnabled(True)
+        self.url_form.setEnabled(True)
+        self.url_form.setText("")
+        self.update_statusbar("اكتمل التحميل!")
+
     def run(self) -> None:
         self.download.setDisabled(True)
         self.url_form.setDisabled(True)
         if not self.url_form.text():
             self.show_error_message("لم تدخل رابط الكتاب بعد!")
             self.download.setEnabled(True)
+            self.url_form.setDisabled(False)
             return
-        downloader = BookDownloader(self.url_form.text())
+        downloader = QBookDownloader(self.url_form.text())
         if not downloader.valid:
             self.show_error_message("رابط غير صحيح!")
             self.download.setEnabled(True)
@@ -122,24 +156,14 @@ class App(QMainWindow):
             self.download.setEnabled(True)
             return
         self.update_statusbar("تحليل معلومات الرابط")
-        # Create a worker object
-        self.worker = Worker(downloader, output)
-        # Move worker to the thread
-        self.worker.moveToThread(self.thread)
+        # Create a qrunner and start it in a thread
+        worker = Worker(downloader, output)
+        self.thread_pool.start(worker)
         # Connect signals and slots
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.worker.progress.connect(self.report_progress)
-        self.worker.downloaded.connect(self.on_process_complete)
-        # Start the thread
-        self.thread.start()
+        worker.signals.progress.connect(self.report_progress)
+        worker.signals.downloaded.connect(self.on_process_complete)
         # Final resets
-        self.thread.finished.connect(lambda: self.download.setEnabled(True))
-        self.thread.finished.connect(lambda: self.url_form.setEnabled(True))
-        self.thread.finished.connect(lambda: self.url_form.setText(""))
-        self.thread.finished.connect(lambda: self.update_statusbar("اكتمل التحميل!"))
+        worker.signals.finished.connect(self.on_finish)
 
     def center(self) -> None:
         """Dynamically center the window in screen."""
