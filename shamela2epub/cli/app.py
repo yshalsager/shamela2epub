1f47151aa0b2ee7f3f0869cd3869a7b6ed7696c5
yshalsager <ysh-alsager@hotmail.com>
2024-03-15 14:19:50 +0200

    feat(cli): skip downloading already existing files and add -f to override
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- shamela2epub/cli/app.py
+++ shamela2epub/cli/app.py
@@ -1,7 +1,9 @@
 import logging
+from pathlib import Path
 
 import click
 
+from shamela2epub import OUT_DIR
 from shamela2epub.main import BookDownloader
 
 logger = logging.getLogger(__name__)
@@ -9,9 +11,12 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument("url", type=str)
-@click.option("-o", "--output", type=str, help="ePub output book custom name")
+@click.option("-o", "--output", type=str, help="ePub output book custom name", default="")
 @click.option("-x", "--connections", type=int, default=16, help="Max number of connections")
-def download(url: str, output: str, connections: int) -> None:
+@click.option(
+    "-f", "--force", is_flag=True, default=False, help="Force download even if the file exists"
+)
+def download(url: str, output: str, connections: int, force: bool) -> None:
     """Download Shamela book form URL to ePub."""
     downloader = BookDownloader(url, connections)
     if not downloader.valid:
@@ -19,9 +24,18 @@ def download(url: str, output: str, connections: int) -> None:
         return
     logger.info(f"Got valid URL: {url}")
     downloader.create_info_page()
-    logger.info(
-        f"Working on book {downloader.book_info_page.title} by {downloader.book_info_page.author}"
+    book_name = f"{downloader.book_info_page.title} - {downloader.book_info_page.author}"
+    logger.info(f"Working on book {book_name}")
+    output_path = (
+        output
+        if output.endswith(".epub")
+        else f"{output}/{book_name}.epub"
+        if output
+        else f"{OUT_DIR}/{book_name}.epub"
     )
+    if Path(output_path).exists() and not force:
+        logger.info("The file already exists! Exiting...")
+        return
     downloader.download()
     # Save new book
     logger.info("Saving the new book")
